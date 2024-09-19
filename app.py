f6fd11be4a9f28ccba174cea92a90ca7674abc65
Mészáros Róbert <mrobert3456@gmail.com>
2024-09-19 17:36:23 +0200

    lambda email sender init

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
 from fastapi import FastAPI
+from send_mail import send_email
+import json
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+
+class EmailContent(BaseModel):
+     name:str
+     email:str
+     message:str
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+]
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/sendEmail")
+async def send_email(EmailContent:EmailContent):
+    return {
+        'statusCode': 200,
+        'body': json.dumps("Email Sent Successfully")
+    }
+    
--- send_mail.py
+++ send_mail.py
@@ -1,4 +1,53 @@
+import json
+import boto3
+import os
+from dotenv import load_dotenv
+from botocore.exceptions import ClientError
 
-def send_email():
+load_dotenv()
 
-   pass
+def send_email(name, email, message):
+    client = boto3.client('ses', region_name=os.getenv("AWS_REGION"), aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"), aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"))    
+    
+    try:    
+        client.send_email(
+            Destination={
+                'ToAddresses': ["meszarosr734@gmail.com"]
+            },
+            Message={
+                'Body': {
+                    'Text': {
+                        'Charset': 'UTF-8',
+                        'Data': message,
+                    }
+                },
+                'Subject': {
+                    'Charset': 'UTF-8',
+                    'Data': f"{name} - {email} wants to connect",
+                },
+            },
+            Source='contact@robert-meszaros.com'
+            )
+        
+    except ClientError as e:
+        return {
+            'statusCode': 400,
+            'body': e.response['Error']['Message']
+        }
+    
+    else:
+        return {
+        'statusCode': 200,
+        'body': json.dumps("Email Sent Successfully")
+    }
+
+def lambda_handler(event, context):
+    try:
+        return send_email(event["name"], event["email"], event["message"])
+    except Exception as e:
+        return {
+            'statusCode': 400,
+            'body': json.dumps(f"error: {e.message}")   
+        }
+    
+    
